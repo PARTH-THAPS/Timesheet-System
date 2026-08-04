364f662dc71d31829e6008a60ebb5d7e5fa7e703
Areeba Naseer <areeba@MacBookPro.fritz.box>
2026-08-04 13:32:21 +0200

    Implemented the calculateHoursDue function

--- BRAVO-TSS-ejb/src/java/tss/entity/Timesheet.java
+++ BRAVO-TSS-ejb/src/java/tss/entity/Timesheet.java
@@ -5,9 +5,11 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+
 
 @Entity
 public class Timesheet extends AbstractEntity {
@@ -33,16 +35,44 @@ public class Timesheet extends AbstractEntity {
             this.status = TimesheetStatus.IN_PROGRESS;
             this.startDate = startDate;
             this.endDate = endDate;
+            this.contract = contract;
             this.hoursDue = calculateHoursDue();
             this.signedByEmployee = null;
             this.signedBySupervisor = null;
             this.entries = new ArrayList<>();
-            this.contract = contract;
+            
     }
-    
+
     private double calculateHoursDue() {
-        //TODO: Implement
-        return 10;
+        int workingDaysInPeriod = countWorkingDays(startDate, endDate);
+        int publicHolidaysInPeriod = 0; // TODO: Compute Public Holiday
+        
+
+        double hoursPerWeek = contract.getHoursPerWeek();
+        int workingDaysPerWeek = contract.getWorkingDaysPerWeek();
+
+        return (workingDaysInPeriod - publicHolidaysInPeriod) * hoursPerWeek / workingDaysPerWeek;
+    }
+
+    private int countWorkingDays(LocalDate startDate, LocalDate endDate) {
+        int count = 0;
+        LocalDate current = startDate;
+
+        while (!current.isAfter(endDate)) {
+            if (isWorkingDay(current)) {
+                count++;
+            }
+            current = current.plusDays(1);
+        }
+        
+        return count;
+        
+    }
+
+
+    private boolean isWorkingDay(LocalDate date) {
+        DayOfWeek day = date.getDayOfWeek();
+        return day != DayOfWeek.SATURDAY && day != DayOfWeek.SUNDAY;
     }
 
     public TimesheetStatus getStatus() {
@@ -80,6 +110,10 @@ public class Timesheet extends AbstractEntity {
     public void setSignedBySupervisor(LocalDate signedBySupervisor) {
         this.signedBySupervisor = signedBySupervisor;
     }
+    
+    public Contract getContract() {
+        return contract;
+    }
 
     public List<TimesheetEntry> getEntries() {
         return entries;
@@ -94,4 +128,6 @@ public class Timesheet extends AbstractEntity {
         this.entries.remove(entry);
         return entries;
     }
+    
+   
 }
